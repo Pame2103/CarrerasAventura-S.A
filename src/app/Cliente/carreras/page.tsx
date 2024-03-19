e575c98de5768d96f853e291a8e2f4ec2db6e730
Pamela2103 <128122889+Pamela2103@users.noreply.github.com>
2024-03-18 23:20:25 -0600

    cambios

--- src/app/Cliente/carreras/page.tsx
+++ src/app/Cliente/carreras/page.tsx
@@ -3,27 +3,28 @@ import React, { useState, useEffect } from 'react';
 import Navbar from '@/app/componentes/navbar';
 import { collection, onSnapshot, doc, getDoc } from 'firebase/firestore';
 import { db } from '../../../../firebase/firebase';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 
 interface Carrera {
   id: string;
-  nombre: string;
-  fecha: string;
-  costo: string;
-  distancia: string;
+  evento: string;
   edicion: string;
- limiteParticipante:string;
+  fecha: string;
+  distancia: string;
+  tipocarrera: string;
+  estadocarrera: string;
+  costo: string;
   responsable: string;
   contacto: string;
-  lugar: string;
-  hora: string;
-  cupo: number; // Se cambia a number
+  cupo: number;
+  nombre: string; // Asegúrate de que 'nombre' está definido en la interfaz Carrera
 }
 
 function groupByMonth(carreras: Carrera[]): { [key: string]: Carrera[] } {
   const grouped: { [key: string]: Carrera[] } = {};
 
   carreras.forEach(carrera => {
+    // Ensure carrera.fecha exists and is a string
     if (typeof carrera.fecha === 'string' && carrera.fecha.includes('-')) {
       const parts = carrera.fecha.split('-');
       const month = new Date(2022, parseInt(parts[1]) - 1, 1).toLocaleString('default', { month: 'long' });
@@ -39,14 +40,14 @@ function groupByMonth(carreras: Carrera[]): { [key: string]: Carrera[] } {
   return grouped;
 }
 
-async function getCupo(carreraId: string): Promise<number> { // Se especifica que la función devuelve un número
+async function getCupo(carreraId: string) {
   const carreraDocRef = doc(db, 'Configuracion Carreeras', carreraId);
   const carreraDocSnap = await getDoc(carreraDocRef);
   if (carreraDocSnap.exists()) {
     return carreraDocSnap.data().limiteParticipantes;
   } else {
     console.error(`Carrera document with ID ${carreraId} not found.`);
-    return 0;
+    return 0; // Return 0 if document not found
   }
 }
 
@@ -58,10 +59,10 @@ function Carreras() {
   useEffect(() => {
     const carrerasRef = collection(db, 'Configuracion Carreeras');
     const unsubscribe = onSnapshot(carrerasRef, (querySnapshot) => {
-      const carrerasData: Carrera[] = querySnapshot.docs.map(doc => ({
+      const carrerasData = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-      })) as Carrera[];
+      }) as Carrera);
       setCarreras(carrerasData);
     });
     return () => unsubscribe();
@@ -72,33 +73,23 @@ function Carreras() {
     setCarrerasByMonth(groupedCarreras);
   }, [carreras]);
 
-  const handleInscribirse = async (carreraId: string, eventoNombre: string) => {
+  const handleInscribirse = async (carreraId: string) => {
     const cupo = await getCupo(carreraId);
-    console.log("Cupo actual:", cupo); // Agregar este console.log
-    const raceToUpdateIndex = carreras.findIndex(carrera => carrera.id === carreraId);
-    console.log("Index de la carrera:", raceToUpdateIndex); // Agregar este console.log
-    
-    if (raceToUpdateIndex !== -1) {
+    const raceToUpdate = carreras.find(carrera => carrera.id === carreraId);
+    if (raceToUpdate) {
       if (cupo > 0) {
-        const updatedCarreras = [...carreras];
-        updatedCarreras[raceToUpdateIndex] = {
-          ...updatedCarreras[raceToUpdateIndex],
-          cupo: cupo - 1
-        };
-        setCarreras(updatedCarreras); // Actualizar el estado aquí
-
-        localStorage.setItem('carreraSeleccionada', JSON.stringify(updatedCarreras[raceToUpdateIndex]));
-        localStorage.setItem('nombreEventoSeleccionado', eventoNombre);
-        window.location.href = '/Cliente/Inscripciones';
+        const updatedCarreras = carreras.map(carrera =>
+          carrera.id === carreraId ? { ...carrera, cupo: cupo - 1 } : carrera
+        );
+        setCarreras(updatedCarreras);
+        console.log(`Inscribiéndose en la carrera con ID ${carreraId}`);
       } else {
         setMensaje("El cupo de participantes ya ha llegado a su límite. Gracias por su interés, por favor revise otros eventos disponibles.");
       }
-    } else {
-      console.error(`No se encontró la carrera con ID ${carreraId} en el estado.`);
     }
   };
 
-  const today = new Date(); 
+  const today = new Date(); // Obtener la fecha actual
 
   return (
     <>
@@ -129,26 +120,35 @@ function Carreras() {
                       <div key={carrera.id} className="bg-white rounded-lg shadow-md p-4 mb-4 w-full max-w-3xl">
                         <div className="grid grid-cols-3 gap-4">
                           <div>
-                            <h3 className="text-lg font-bold">{carrera.nombre}</h3> {/* Se cambia a carrera.nombre */}
+                            <h3 className="text-lg font-bold">{carrera.nombre}</h3> {/* Añadido el nombre de la carrera */}
+                            <p>{carrera.evento}</p>
                             <p>{carrera.edicion}</p>
                             <p>{carrera.fecha}</p>
                             <p>Cupos Disponibles: {carrera.cupo}</p>
                           </div>
                           <div>
                             <p>Distancia: {carrera.distancia}</p>
-                        
+                            <p>Tipo de Carrera: {carrera.tipocarrera}</p>
+                            <p>Estado de la Carrera: {carrera.estadocarrera}</p>
                           </div>
-                          <div className="flex flex-col items-center justify-center">
-                            <button
-                              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${carrera.cupo <= 0 ? 'bg-gray-300 cursor-not-allowed' : ''}`}
-                              onClick={() => {
-                                console.log("Clic en el botón de inscripción:", carrera.id, carrera.nombre);
-                                carrera.cupo > 0 && handleInscribirse(carrera.id, carrera.nombre)
-                              }} 
-                              disabled={carrera.cupo <= 0}
-                            >
-                              {carrera.cupo > 0 ? 'Inscribirse' : 'Cupo lleno'}
-                            </button>
+                          <div>
+                            <p>Costo: {carrera.costo}</p>
+                            <p>Responsable: {carrera.responsable}</p>
+                            <p>Contacto: {carrera.contacto}</p>
+                            <p style={{ color: carrera.cupo > 0 ? 'black' : 'red' }}>
+                              {carrera.cupo > 0 ? (
+                                <a href={`/Cliente/Inscripciones`}>
+                                  <button
+                                    style={{ backgroundColor: '#007bff', color: '#fff', border: 'none', padding: '5px 10px', cursor: 'pointer', margin: 'auto' }}
+                                    onClick={() => handleInscribirse(carrera.id)}
+                                  >
+                                    Inscribirse
+                                  </button>
+                                </a>
+                              ) : (
+                                "El cupo de participantes está lleno."
+                              )}
+                            </p>
                           </div>
                         </div>
                       </div>
@@ -158,8 +158,8 @@ function Carreras() {
               }
               return null;
             })}
-          {mensaje && <p>{mensaje}</p>}
         </div>
+        {mensaje && <div>{mensaje}</div>}
       </div>
     </>
   );
