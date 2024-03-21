3de8054d9b61d1aa089d5944b7b79979f485a131
Pamela2103 <128122889+Pamela2103@users.noreply.github.com>
2024-03-21 11:14:30 -0600

    cambios

--- src/app/Admin/configuraciones/page.tsx
+++ src/app/Admin/configuraciones/page.tsx
@@ -1,15 +1,10 @@
-
 'use client'
 import React, { useState, useEffect } from 'react';
-
 import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '../../../../firebase/firebase';
-import { doc, deleteDoc } from 'firebase/firestore'
-import {  updateDoc } from 'firebase/firestore';
-import {  addDoc, } from 'firebase/firestore';
-
-
-
+import { doc, deleteDoc } from 'firebase/firestore';
+import { updateDoc } from 'firebase/firestore';
+import { addDoc } from 'firebase/firestore';
 
 interface Administrador {
   id: string;
@@ -29,8 +24,8 @@ function Configuraciones() {
   useEffect(() => {
     const obtenerAdministradoresDesdeFirebase = async () => {
       try {
-        const querySnapshot = await onSnapshot(collection(db, 'Usuarios'), (snapshot) => {
-          const administradoresData: Administrador[] = snapshot.docs.map((doc) => ({
+        const querySnapshot = await onSnapshot(collection(db, 'Usuarios'), snapshot => {
+          const administradoresData: Administrador[] = snapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data(),
           } as Administrador));
@@ -46,18 +41,18 @@ function Configuraciones() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     const newAdmin: Omit<Administrador, 'id'> = {
       nombre,
       email,
       usuario: apellidos,
       password: hashPassword(password),
     };
-  
+
     try {
       const docRef = await addDoc(collection(db, 'Usuarios'), newAdmin);
       console.log('Datos del usuario agregados con ID: ', docRef.id);
-  
+
       // Actualizar state con el nuevo administrador y el ID generado por Firebase
       setAdministradores([...administradores, { id: docRef.id, ...newAdmin }]);
       setNombre('');
@@ -73,8 +68,8 @@ function Configuraciones() {
     try {
       const userDocRef = doc(db, 'Usuarios', id);
       await deleteDoc(userDocRef);
-  
-      const nuevosAdministradores = administradores.filter((admin) => admin.id !== id);
+
+      const nuevosAdministradores = administradores.filter(admin => admin.id !== id);
       setAdministradores(nuevosAdministradores);
     } catch (error) {
       console.error('Error al eliminar el administrador:', error);
@@ -84,16 +79,16 @@ function Configuraciones() {
   const handleEditar = async (id: string) => {
     try {
       const userDocRef = doc(db, 'Usuarios', id);
-      const administradorEditado = administradores.find((admin) => admin.id === id);
-  
+      const administradorEditado = administradores.find(admin => admin.id === id);
+
       await updateDoc(userDocRef, {
         nombre: nombre || administradorEditado?.nombre,
         email: email || administradorEditado?.email,
         usuario: apellidos || administradorEditado?.usuario,
         password: hashPassword(password) || administradorEditado?.password,
       });
-  
-      const nuevosAdministradores = administradores.map((admin) => {
+
+      const nuevosAdministradores = administradores.map(admin => {
         if (admin.id === id) {
           return {
             ...admin,
@@ -105,13 +100,13 @@ function Configuraciones() {
         }
         return admin;
       });
-  
+
       setAdministradores(nuevosAdministradores);
     } catch (error) {
       console.error('Error al editar el administrador:', error);
     }
   };
-  
+
   const hashPassword = (password: string) => {
     // Implementa tu lógica para hashear la contraseña aquí
     return password;
@@ -119,93 +114,166 @@ function Configuraciones() {
 
   return (
     <>
-    
-      <div className="text-black min-h-screen" style={{ fontFamily: 'Arial', color: '#3c78f2', backgroundColor: '#E0E6F3' }}>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-0 right-0 mt-4 mr-4"
-          onClick={() => window.history.back()}
-        >
-          Volver
-        </button>
-        <h1 style={{ textAlign: 'center', fontSize: '30px', color: 'black' }}>ADMINISTRADORES</h1>
+      <style>
+        {`
+        body {
+          margin: 0;
+          padding: 0;
+          background-image: url('/per.jpg');
+          background-size: cover;
+          background-repeat: no-repeat;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-        <br />
-        <br />
-        <br />
+        .container {
+          max-width: 900px;
+          padding: 20px;
+          border: 1px solid #ccc;
+          border-radius: 8px;
+          background: rgba(255, 255, 255, 0.8); /* Agregar fondo blanco semi-transparente para mejorar la legibilidad */
+        }
+          .form-input {
+            padding: 8px;
+            border-radius: 4px;
+            border: 1px solid #ccc;
+            margin-bottom: 10px;
+            width: 100%;
+          }
+
+          .btn {
+            padding: 8px;
+            border-radius: 4px;
+            background-color: #3c78f2;
+            color: white;
+            border: none;
+          }
+
+          .table-container {
+            overflow-x: auto;
+          }
+
+          .table {
+            width: 100%;
+            border-collapse: collapse;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            background-color: white;
+          }
+
+          .table th,
+          .table td {
+            border: 1px solid #ccc;
+            padding: 8px;
+            text-align: left;
+          }
+
+          .table th {
+            background-color: #B1CEE3;
+          }
+
+          .table tbody tr:hover {
+            background-color: #f0f0f0;
+          }
+
+          .editar,
+          .eliminar {
+            cursor: pointer;
+            margin-right: 5px;
+            padding: 5px 10px;
+            border-radius: 4px;
+            border: none;
+          }
+
+          .editar {
+            background-color: #1D6CE6;
+            color: white;
+          }
+
+          .eliminar {
+            background-color: #1D6CE6;
+            color: white;
+          }
+        `}
+      </style>
+      <div className="container">
+        <h1 style={{ textAlign: 'center', fontSize: '30px', color: 'black' }}>Configuaracion Usuarios</h1>
         <form onSubmit={handleSubmit}>
           <label htmlFor="nombre">Nombre:</label>
           <input
             type="text"
             id="nombre"
             value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
+            onChange={e => setNombre(e.target.value)}
             required
+            className="form-input"
           />
-
           <label htmlFor="apellidos">Apellidos:</label>
           <input
             type="text"
             id="apellidos"
             value={apellidos}
-            onChange={(e) => setApellidos(e.target.value)}
+            onChange={e => setApellidos(e.target.value)}
             required
+            className="form-input"
           />
-
           <label htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value)}
             required
+            className="form-input"
           />
-
           <label htmlFor="password">Contraseña:</label>
           <input
             type="password"
             id="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={e => setPassword(e.target.value)}
             required
+            className="form-input"
           />
-
-          <button type="submit">Agregar Administrador</button>
+          <button type="submit" className="btn">Agregar Administrador</button>
         </form>
-
-        <div style={{ overflowX: 'auto' }}>
-          <table className="table-auto w-full border-collapse border border-gray-300 shadow-lg rounded">
-            <thead style={{ backgroundColor: '#B1CEE3' }}>
+        <div className="table-container">
+          <table className="table">
+            <thead>
               <tr>
-                <th className="border px-4 py-2">Nombre</th>
-                <th className="border px-4 py-2">Apellidos</th>
-                <th className="border px-4 py-2">Email</th>
-                <th className="border px-4 py-2">Contraseña</th>
-                <th className="border px-4 py-2">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {administradores.map((admin) => (
-                <tr key={admin.id} className="hover:bg-gray-100">
-                  <td className="border px-4 py-2">{admin.nombre}</td>
-                  <td className="border px-4 py-2">{admin.usuario}</td>
-                  <td className="border px-4 py-2">{admin.email}</td>
-                  <td className="border px-4 py-2">{admin.password}</td>
-                  <td className="border px-4 py-2 text-center">
-                    <button className="editar" onClick={() => handleEditar(admin.id)}>
-                      Editar
-                    </button>
-                    <button className="eliminar" onClick={() => handleEliminar(admin.id)}>
-                      Eliminar
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
-  );
+                <th>Nombre</th>
+                <th>Apellidos</th>
+                <th>Email</
+                th>
+<th>Contraseña</th>
+<th>Acciones</th>
+</tr>
+</thead>
+<tbody>
+{administradores.map(admin =>
+<tr key={admin.id}>
+<td>{admin.nombre}</td>
+<td>{admin.usuario}</td>
+<td>{admin.email}</td>
+<td>{admin.password}</td>
+<td>
+<button className="editar" onClick={() => handleEditar(admin.id)}>
+Editar
+</button>
+<button className="eliminar" onClick={() => handleEliminar(admin.id)}>
+Eliminar
+</button>
+</td>
+</tr>
+)}
+</tbody>
+</table>
+</div>
+</div>
+</>
+);
 }
 
 export default Configuraciones;
