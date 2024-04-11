23b4029e8bbe9c3ce2922093b23b640b2db327a8
Pamela2103 <128122889+Pamela2103@users.noreply.github.com>
2024-04-11 01:40:36 -0600

    cambios

--- src/app/Admin/administradorCarreras/page.tsx
+++ src/app/Admin/administradorCarreras/page.tsx
@@ -1,8 +1,10 @@
 'use client'
-import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
-import { TextField, Button, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { TextField, Button, InputLabel } from '@mui/material';
+import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../../../../firebase/firebase';
-import { collection, getDocs, doc, updateDoc, query, where, doc as docRef } from 'firebase/firestore';
+import Link from 'next/link';
+import { FaRunning, FaInfoCircle, FaDumbbell, FaEnvelope, FaTrophy, FaSignInAlt } from 'react-icons/fa';
 
 interface Carrera {
   nombre: string;
@@ -36,35 +38,9 @@ function Administradorcarreras() {
   const [operacionExitosa, setOperacionExitosa] = useState<boolean | null>(null);
   const [errorMessage, setErrorMessage] = useState<string>('');
 
-  const [carreras, setCarreras] = useState<Carrera[]>([]);
-  const [selectedCarrera, setSelectedCarrera] = useState<string>('');
-
-  useEffect(() => {
-    async function fetchCarreras() {
-      const q = query(collection(db, 'Configuracion Carreeras'));
-      const carrerasSnapshot = await getDocs(q);
-      const carrerasData: Carrera[] = [];
-      carrerasSnapshot.forEach((doc) => {
-        carrerasData.push(doc.data() as Carrera);
-      });
-      setCarreras(carrerasData);
-    }
-    fetchCarreras();
-  }, []);
-
   const handleChange = (event: ChangeEvent<{ name?: string; value: unknown }>) => {
     const { name, value } = event.target;
     setNuevaCarrera({ ...nuevaCarrera, [name as string]: value as string });
-  };
-
-  const handleSelectChange = (event: SelectChangeEvent<string>) => {
-    const selectedCarreraNombre = event.target.value;
-    setSelectedCarrera(selectedCarreraNombre);
-
-    const carrera = carreras.find(carrera => carrera.nombre === selectedCarreraNombre);
-    if (carrera) {
-      setNuevaCarrera(carrera);
-    }
   };
 
   const handleSubmit = async (e: FormEvent) => {
@@ -79,61 +55,109 @@ function Administradorcarreras() {
     } catch (error) {
       console.error('Error updating form data in Firebase:', error);
       setOperacionExitosa(false);
-      setErrorMessage('Hubo un error al agregar los cambios.');
+      setErrorMessage('Hubo un error al agregar la nueva carrera.');
     }
   };
 
-  const updateCarreraInFirebase = async (updatedCarrera: Carrera) => {
+  const updateCarreraInFirebase = async (newCarrera: Carrera) => {
     try {
-      const q = query(collection(db, 'Configuracion Carreeras'), where('nombre', '==', updatedCarrera.nombre));
-      const querySnapshot = await getDocs(q);
-      
-      if (!querySnapshot.empty) {
-        querySnapshot.forEach(async (doc) => {
-          try {
-            const carreraRef = docRef(db, `Configuracion Carreeras/${doc.id}`);
-            // Copiar todas las propiedades de Carrera excepto 'nombre'
-            const { nombre, ...updatedCarreraData } = updatedCarrera;
-            await updateDoc(carreraRef, updatedCarreraData);
-            console.log('Carrera updated:', updatedCarrera.nombre);
-          } catch (error) {
-            console.error('Error updating carrera:', error);
-            throw error;
-          }
-        });
-      } else {
-        console.error('Documento no encontrado:', updatedCarrera.nombre);
-        throw new Error('Documento no encontrado en la base de datos.');
-      }
+      const carreraCollectionRef = collection(db, 'Configuracion Carreeras');
+      await addDoc(carreraCollectionRef, newCarrera);
+      console.log('Nueva carrera agregada:', newCarrera.nombre);
     } catch (error) {
-      console.error('Error querying database:', error);
+      console.error('Error adding new carrera:', error);
       throw error;
     }
   };
 
+  const Navbar: React.FC = () => {
+    return (
+      <nav className="bg-white border-b border-gray-200 fixed w-full z-23 top-0 left-0 h-23">
+        <div className="max-w-screen-2xl mx-auto px-6 sm:px-7 lg:px-9">
+          <div className="flex items-center justify-between h-full">
+            <div className="flex items-center">
+              <Link href="/">
+                <img src="/LogoC.png" className="h-20 w-auto" alt="Carrera Aventura" />
+              </Link>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <Link href="/Admin/administradorCarreras">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaRunning className="mr-1" /> Administrar Carreras
+                    </span>
+                  </Link>
+                  <Link href="/Admin/administrarTiempos">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaInfoCircle className="mr-1" /> Administrar Tiempos
+                    </span>
+                  </Link>
+                  <Link href="/Admin/carreras">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaDumbbell className="mr-1" /> Carreras
+                    </span>
+                  </Link>
+                  <Link href="/Admin/confirmaciones">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaTrophy className="mr-1" />Confirmación de Pagos
+                    </span>
+                  </Link>
+                  <Link href="/Admin/historicosadmin">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaInfoCircle className="mr-1" /> Históricos
+                    </span>
+                  </Link>
+                  <Link href="/Admin/listaParticipantes">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaTrophy className="mr-1" /> Lista de Participantes
+                    </span>
+                  </Link>
+                  <Link href="/Admin/record">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaEnvelope className="mr-1" /> Records
+                    </span>
+                  </Link>
+                  <Link href="/Admin/resultados">
+                    <span className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium flex items-center">
+                      <FaEnvelope className="mr-1" /> Resultados
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="flex">
+              <Link href="/Login" className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md font-medium flex items-center">
+                <FaSignInAlt className="mr-2" /> Cerrar sesión
+              </Link>
+            </div>
+          </div>
+          <div className="ml-10 text-gray-600 text-sm font-medium">¡Corre hacia tus metas con Carrera Aventura! ¡Cruzando la meta juntos!</div>
+        </div>
+      </nav>
+    );
+  }
+
   return (
     <>
-      {/* Estilos */}
       <style>
         {`
           body {
             margin: 0;
             padding: 0;
-            background-colorblue
-            background-size: cover;
+            background-color: blue--700;
+            background-size: blue--700;
             background-repeat: no-repeat;
-            height: 100vh;
+            height: 200vh;
             display: flex;
             justify-content: center;
             align-items: center;
           }
           
           .container {
-            max-width: 800px;
+            max-width: 900px;
             padding: 20px;
             border: 1px solid #ccc;
-            border-radius: 8px;
-            background: rgba(255, 255, 255, 0.8); /* Agregar fondo blanco semi-transparente para mejorar la legibilidad */
+            border-radius: 5px;
+            background: rgba(250, 250, 250, 0.6);
           }
           
           .form-container {
@@ -142,12 +166,13 @@ function Administradorcarreras() {
           
           .form-row {
             display: flex;
+            flex-wrap: wrap;
             justify-content: space-between;
-            margin-bottom: 16px; /* Se corrigió la propiedad margin-bottom */
+            margin-bottom: 16px;
           }
           
           .form-field {
-            flex: 0 0 calc(33.33% - 8px);
+            flex: 0 0 calc(50% - 8px); /* Cambiado el ancho para que los campos se muestren en dos columnas */
           }
           
           .form-container form div {
@@ -162,17 +187,15 @@ function Administradorcarreras() {
         `}
       </style>
 
-      {/* Contenido del componente */}
       <div className="container">
-        <h2 style={{ textAlign: 'center', fontSize: '2em', fontWeight: 'bold' }}>Administrador de carreras</h2>
+        <br />
+        <br />
+        <br />
+        <h1 style={{ fontSize: '3rem', fontWeight: 'bold', marginBottom: '2rem', color: '#333', textAlign: 'center' }}>CREAR EVENTO</h1>
         <div className="form-container">
+          <br />
+          <br />
           <form onSubmit={handleSubmit}>
-            <Select value={selectedCarrera} onChange={handleSelectChange} fullWidth>
-              <MenuItem value="">Seleccionar Carrera</MenuItem>
-              {carreras.map(carrera => (
-                <MenuItem key={carrera.nombre} value={carrera.nombre}>{carrera.nombre}</MenuItem>
-              ))}
-            </Select>
             <div className="form-row">
               <div className="form-field">
                 <InputLabel>Nombre:</InputLabel>
@@ -198,6 +221,8 @@ function Administradorcarreras() {
                   placeholder='3 TH'
                 />
               </div>
+            </div>
+            <div className="form-row">
               <div className="form-field">
                 <InputLabel>Fecha:</InputLabel>
                 <TextField
@@ -210,7 +235,45 @@ function Administradorcarreras() {
                   placeholder='2024/10/31'
                 />
               </div>
+              <div className="form-field">
+                <InputLabel>Responsable:</InputLabel>
+                <TextField
+                  label=''
+                  variant='outlined'
+                  name='responsable'
+                  value={nuevaCarrera.responsable}
+                  onChange={handleChange}
+                  fullWidth
+                  placeholder='Juan Mora'
+                />
               </div>
+            </div>
+            <div className="form-row">
+              <div className="form-field">
+                <InputLabel>Contacto:</InputLabel>
+                <TextField
+                  label=''
+                  variant='outlined'
+                  name='contacto'
+                  value={nuevaCarrera.contacto}
+                  onChange={handleChange}
+                  fullWidth
+                  placeholder='Carreraschirripo@gmail.com'
+                />
+              </div>
+              <div className="form-field">
+                <InputLabel>Lugar:</InputLabel>
+                <TextField
+                  label=''
+                  variant='outlined'
+                  name='lugar'
+                  value={nuevaCarrera.lugar}
+                  onChange={handleChange}
+                  fullWidth
+                  placeholder='Cerro chirripo ,Perez Zeledon, San Jose,Costa Rica'
+                />
+              </div>
+            </div>
             <div className="form-row">
               <div className="form-field">
                 <InputLabel>Distancia:</InputLabel>
@@ -224,6 +287,20 @@ function Administradorcarreras() {
                   placeholder='11 KM'
                 />
               </div>
+              <div className="form-field">
+                <InputLabel>Limite Participantes:</InputLabel>
+                <TextField
+                  label=''
+                  variant='outlined'
+                  name='limiteParticipante'
+                  value={nuevaCarrera.limiteParticipante}
+                  onChange={handleChange}
+                  fullWidth
+                  placeholder='50'
+                />
+              </div>
+            </div>
+            <div className="form-row">
               <div className="form-field">
                 <InputLabel>Cupos Disponibles:</InputLabel>
                 <TextField
@@ -251,30 +328,6 @@ function Administradorcarreras() {
             </div>
             <div className="form-row">
               <div className="form-field">
-                <InputLabel>Responsable:</InputLabel>
-                <TextField
-                  label=''
-                  variant='outlined'
-                  name='responsable'
-                  value={nuevaCarrera.responsable}
-                  onChange={handleChange}
-                  fullWidth
-                  placeholder='Juan Mora'
-                />
-              </div>
-              <div className="form-field">
-                <InputLabel>Contacto:</InputLabel>
-                <TextField
-                  label=''
-                  variant='outlined'
-                  name='contacto'
-                  value={nuevaCarrera.contacto}
-                  onChange={handleChange}
-                  fullWidth
-                  placeholder='Carreraschirripo@gmail.com'
-                />
-              </div>
-              <div className="form-field">
                 <InputLabel>Hora:</InputLabel>
                 <TextField
                   label=''
@@ -287,37 +340,20 @@ function Administradorcarreras() {
                 />
               </div>
             </div>
-            <div className="form-row">
-              <div className="form-field">
-                <InputLabel>Lugar:</InputLabel>
-                <TextField
-                  label=''
-                  variant='outlined'
-                  name='lugar'
-                  value={nuevaCarrera.lugar}
-                  onChange={handleChange}
-                  fullWidth
-                  placeholder='Cerro chirripo ,Perez Zeledon, San Jose,Costa Rica'
-                />
-              </div>
-            </div>
-            {/* Botón de envío */}
             <div style={{ display: 'flex', justifyContent: 'center' }}>
               <Button variant='contained' type='submit' color='primary' className='botones'>
-                Agregar Cambios
+                Agregar Carrera
               </Button>
             </div>
           </form>
-
-          {/* Mensajes de éxito o error */}
           {operacionExitosa === true && (
             <div style={{ color: 'green', marginTop: '10px' }}>
-              Los cambios se realizaron con éxito.
+              La carrera se agregó con éxito.
             </div>
           )}
           {operacionExitosa === false && (
             <div style={{ color: 'red', marginTop: '10px' }}>
-              Hubo un error al agregar los cambios.
+              Hubo un error al agregar la carrera.
             </div>
           )}
           {errorMessage && (
@@ -327,6 +363,7 @@ function Administradorcarreras() {
           )}
         </div>
       </div>
+      <Navbar />
     </>
   );
 }
